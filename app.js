9ab93c5feecbacc218bb0119fceb2ded1ab27b3f
sbezboro <sergei.b.ru@gmail.com>
2013-05-25 16:49:00 -0700

    ability to specify different port

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var realtime = require('./realtime')
   , config = require('./config');
 
+if (process.argv.length > 2) {
+  config.port = process.argv[2];
+}
+
 realtime.init(config, function(error) {
   if (error) {
     console.log(error);
