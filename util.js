d6a603b11141531575199e88ec48a14d81a21e17
sbezboro <sergei.b.ru@gmail.com>
2013-12-03 22:23:17 -0800

    fix html encoding

--- util.js
+++ util.js
@@ -1,7 +1,7 @@
 var ansitohtml = require('ansi-to-html');
 
 exports.htmlEncode = function(text) {
-    return text.replace('<', '&lt;').replace('>', '&gt;');
+    return text.replace(/</g, '&lt;').replace(/>/g, '&gt;');
 }
 
 exports.ansiConvert = new ansitohtml();
